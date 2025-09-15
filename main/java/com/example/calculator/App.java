08c8c8b933a1327277a2fa9019df5445bc2638bd
sjww0306 <sjww03@gmail.com>
2025-09-15 20:53:37 +0900

    Feat : 양의 정수를 입력받기
    양의 정수를 입력받도록 변수 선언 및 저장

---
+++ main/java/com/example/calculator/App.java
@@ -0,0 +1,15 @@
+package com.example.calculator;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요 : ");
+        int FirstNum = sc.nextInt(); // 첫번째 입력값 설정
+        System.out.print("두 번째 숫자를 입력하세요 : ");
+        int SecondNum = sc.nextInt(); // 두번째 입력값 설정
+
+    }
+}
