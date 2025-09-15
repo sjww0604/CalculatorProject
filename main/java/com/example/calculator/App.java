e61e2dae5d9df12797884de032edc92b96634f14
sjww0306 <sjww03@gmail.com>
2025-09-15 21:00:22 +0900

    Feat : 사칙연산 입력 기능 추가
    사칙연산 기호를 적절한 타입으로 저장하도록 진행

--- main/java/com/example/calculator/App.java
+++ main/java/com/example/calculator/App.java
@@ -10,6 +10,8 @@ public class App {
         int FirstNum = sc.nextInt(); // 첫번째 입력값 설정
         System.out.print("두 번째 숫자를 입력하세요 : ");
         int SecondNum = sc.nextInt(); // 두번째 입력값 설정
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char opration = sc.next().charAt(0); // 연산기호 입력 및 첫 글자만 추출
 
     }
 }
