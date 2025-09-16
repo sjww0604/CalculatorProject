dbda47769ad88c38bc6a1690fb48de6254aec81d
sjww0306 <sjww03@gmail.com>
2025-09-16 19:48:41 +0900

    Feat : 사칙연산 클래스 연결 및 작동방식 일치화
    Calculator 인스턴스 생성 및 연결 진행
    해당 클래스 내 조건문 수정을 통한 추가 오류 검증기능 추가

--- main/java/com/example/calculator/App.java
+++ main/java/com/example/calculator/App.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        /* Calculator 인스턴스 생성 */
+        Calculator calculator = new Calculator();
         Scanner sc =  new Scanner(System.in);
 
         boolean exit = true; // exit 입력 전 값 초기화 진행, 반복문은 true일 때 실행되도록 설계
@@ -15,31 +17,11 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요: ");
             char opration = sc.next().charAt(0); // 연산기호 입력 및 첫 글자만 추출
 
-            int result = 0; // 계산값 저장 변수 result  설정
-            if (firstNum > 0 && secondNum > 0) {
-                switch (opration) {
-                    case '+':
-                        result = firstNum + secondNum;
-                        System.out.println("결과: " + result);
-                        break;
-                    case '-':
-                        result = firstNum - secondNum;
-                        System.out.println("결과:" + result);
-                        break;
-                    case '*':
-                        result = firstNum * secondNum;
-                        System.out.println("결과:" + result);
-                        break;
-                    case '/':
-                        result = firstNum / secondNum;
-                        System.out.println("결과:" + result);
-                        break;
-                    default:
-                        System.out.println("올바른 사칙연산 기호를 입력하세요");
-                }
-            } else {
-                System.out.println("0 이상의 정수를 입력해주세요!"); // 0 이상의 입력값 검증 진행
-            }
+            Integer result = calculator.calculate(firstNum, secondNum, opration); // Calculator 계산 클래스 적용
+            if(result != null) {
+                System.out.println("결과 : " + result);
+            } // 반환받은 결과값이 null 이 아닌 경우 반환된 result 값을 공유
+
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) ");
             String choice = sc.next().toLowerCase().trim();
             // "exit"라는 글자를 입력받아야하는데 항상 고르게 입력 받아야 하므로 LowerCase(소문자로 치환), trim(공백 제거) 사용
