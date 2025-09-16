010e0273ecf1061a137dfe8e004bd66389898660
sjww0306 <sjww03@gmail.com>
2025-09-16 19:42:19 +0900

    Refactor : 반복문 내 조건문 수정
    Early return 개념을 적용하여 나눗셈 조건 적용
    양의 정수 의미를 통해 초기 입력값 검증 조건문 수정

--- main/java/com/example/calculator/Calculator.java
+++ main/java/com/example/calculator/Calculator.java
@@ -6,36 +6,33 @@ import java.util.List;
 public class Calculator {
      private List<Integer> saveList = new ArrayList<>(); // 값을 저장하는 배열형태의 컬렉션 필드 선언 및 생성
 
-        public int calculate(int firstNum, int secondNum, char operation){ //계산 메서드
-            int result = 0; // 결과값 저장 변수 설정
-
-            switch (operation) {
-                case '+':
-                    result = firstNum + secondNum;
-                    break;
-                case '-':
-                    result = firstNum - secondNum;
-                    break;
-                case '*':
-                    result = firstNum * secondNum;
-                    break;
-                case '/':
-                    if (secondNum != 0) {
-                        result = firstNum / secondNum;
-                    } else {
-                        throw new ArithmeticException("0으로 나눌 수 없습니다."); // 예외 처리에 대한 함수 설정
-                    }
-                    break;
-                default:
-                    throw new IllegalArgumentException("지원하지 않는 연산자입니다." + operation);
+        public Integer calculate(int firstNum, int secondNum, char operation) { //계산 메서드
+            if (firstNum < 0 || secondNum < 0) {
+                System.out.println("양의 정수를 입력하세요.");
+                return null;
             }
-
-            // 연산 결과 컬렉션 저장
-            saveList.add(result);
-
-            // 결과 반환
-            return result;
+            int result = 0; // 결과값 저장 변수 설정
+                switch (operation) {
+                    case '+':
+                        result = firstNum + secondNum;
+                        break;
+                    case '-':
+                        result = firstNum - secondNum;
+                        break;
+                    case '*':
+                        result = firstNum * secondNum;
+                        break;
+                    case '/':
+                        if ( secondNum == 0) {
+                            System.out.println("0으로 나눌 수 없습니다."); // Early return 개념을 확인하여 작성할 것
+                            return null;
+                        }
+                        result = firstNum / secondNum;
+                        break;
+                    default:
+                        System.out.println("올바른 사칙연산 기호를 입력하세요.");
+                        return null;
+                }
+                return result;
         }
-
-    }
-
+ }
