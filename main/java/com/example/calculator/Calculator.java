c5644fe208ae7abd0489437269fe080ad2a797b9
sjww0306 <sjww03@gmail.com>
2025-09-16 17:18:04 +0900

    Feat : Calculator 클래스 생성
    배열형태의 결과값 저장 컬렉션 필드 설정
    
    계산 메서드 선언

---
+++ main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.example.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    public static void main(String[] args) {
+        private List<Integer> saveList = new ArrayList<>(); // 값을 저장하는 배열형태의 컬렉션 필드 선언 및 생성
+
+        public int calculate(int firstNum, int secondNum, char operation) { //계산 메서드
+            int result = 0; // 결과값 저장 변수 설정
+
+
+        }
+
+    }
+}
