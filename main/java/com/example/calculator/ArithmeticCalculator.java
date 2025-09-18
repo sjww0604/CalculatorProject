8ecfeb895c13dc2391ed61a555fd1d7ccfef2b18
sjww0306 <sjww03@gmail.com>
2025-09-18 15:11:36 +0900

    Feat : 람다-스트림 입력값 검증 기능 추가
    입력받은 값 기준으로 큰 값을 배열 형태로 출력하는 기능 추가
    
    최종적으로 App.java 연결을 통한 동작 확인 필요

--- main/java/com/example/calculator/ArithmeticCalculator.java
+++ main/java/com/example/calculator/ArithmeticCalculator.java
@@ -3,28 +3,33 @@ package com.example.calculator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArithmeticCalculator {
+/* 다양한 타입을 받을 수 있도록 제네릭 타입 설정
+단, 입력은 다양한 타입으로 되나 내부적으로는 double 형태로 값을 바꿔 계산
+ */
+public class ArithmeticCalculator<T extends Number> {
     // 속성
-    private List<Integer> saveList = new ArrayList<>(); // 배열 값을 저장하는 객체 생성
+    private List<Double> saveList = new ArrayList<>(); // 배열 값을 저장하는 객체 생성
 
-    public int calculate(int firstNum, int secondNum, Operator operator) {
+    public double calculate(T firstNum, T secondNum, OperatorType operatorType) {
+        double a = firstNum.doubleValue();
+        double b = secondNum.doubleValue();
+        double result;
 
-        int result;
-        switch (operator) {
+        switch (operatorType) {
             case ADD:
-                result = firstNum + secondNum;
+                result = a + b;
                 break;
             case SUBTRACT:
-                result = firstNum - secondNum;
+                result = a - b;
                 break;
             case MULTIPLY:
-                result = firstNum * secondNum;
+                result = a * b;
                 break;
             case DIVIDE:
-                if (firstNum == 0 || secondNum == 0) {
+                if (b == 0) {
                     throw new ArithmeticException("0을 나누거나 0으로 나눌 수 없습니다!");
                 }
-                result = firstNum / secondNum;
+                result = a / b;
                 break;
             default:
                 throw new IllegalArgumentException("지원하지 않는 연산자입니다.");
@@ -33,16 +38,24 @@ public class ArithmeticCalculator {
         return result;
     }
     // 생성자 (결과값 반환 및 배열 저장)
-    public List<Integer> getSaveList() {
+    public List<Double> getSaveList() {
         return saveList;
     }
 
     // 기능 (초기값 삭제기능 유지)
-    public Integer removeResult() {
+    public Double removeResult() {
         if (saveList.isEmpty()) {
             return null;
         }
         return saveList.remove(0);
+    }
+
+    // 입력받은 값보다 큰 값 출력 메서드
+    public List<Double> compareLargeNumPrint (double scNumStandard) {
+        return saveList.stream()
+                .filter(v -> v > scNumStandard) // 람다식 활용 scNumStandard(기준값 = 입력받은 값을 기준으로)
+                .sorted()
+                .toList();
     }
 }
 
