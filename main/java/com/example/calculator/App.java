7caefa86b63d6d760ca727ad034139c677a131df
sjww0306 <sjww03@gmail.com>
2025-09-18 15:50:06 +0900

    Feat : Enum 메서드 추가 및 작동 App.java 수정
    각 변수 및 객체를 double 형으로 받아들일 수 있도록 수정 및 설정

--- main/java/com/example/calculator/App.java
+++ main/java/com/example/calculator/App.java
@@ -4,34 +4,34 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        /* Calculator 인스턴스 생성 */
-        Calculator calculator = new Calculator();
+        ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator();
         Scanner sc =  new Scanner(System.in);
 
         boolean exit = true; // exit 입력 전 값 초기화 진행, 반복문은 true일 때 실행되도록 설계
         while(exit) {
             System.out.print("첫 번째 숫자를 입력하세요 : ");
-            int firstNum = sc.nextInt(); // 첫번째 입력값 설정 -> 변수명 첫글자만 소문자로 변경
+            double firstNum = sc.nextDouble(); // 첫번째 입력값 설정 -> 변수명 첫글자만 소문자로 변경
             System.out.print("두 번째 숫자를 입력하세요 : ");
-            int secondNum = sc.nextInt(); // 두번째 입력값 설정
+            double secondNum = sc.nextDouble(); // 두번째 입력값 설정
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0); // 연산기호 입력 및 첫 글자만 추출
+            OperatorType operatorType = OperatorType.fromChar(operator);
 
-            Integer result = calculator.calculate(firstNum, secondNum, operator); // Calculator 계산 클래스 적용
+            Double result = arithmeticCalculator.calculate(firstNum, secondNum, operatorType); // Calculator 계산 클래스 적용
             if(result != null) {
                 System.out.println("결과 : " + result);
             } // 반환받은 결과값이 null 이 아닌 경우 반환된 result 값을 공유
 
             System.out.println("=============================="); // 배열 구분선 추가
             // 저장된 결과값을 보여주는 배열을 하단에 첨부 getter 기능을 활용
-            System.out.println("저장된 결과값: " + calculator.getSaveList());
+            System.out.println("저장된 결과값: " + arithmeticCalculator.getSaveList());
             System.out.print("명령어를 입력하세요. (del=삭제, exit=종료) : ");
 
             // 삭제 기능 추가 및 종료 조건을 만족하기 위해 항상 입력값을 고르게 입력 받아야 하므로 LowerCase(소문자로 치환), trim(공백 제거) 사용
             String choice = sc.next().toLowerCase().trim();
             // 조건문에 del 인 경우의 삭제 메서드 기능 추가 진행
             if (choice.equals("del")) {
-                Integer removeResult = calculator.removeResult();
+                Double removeResult = arithmeticCalculator.removeResult();
                 if (removeResult != null) {
                     System.out.println("삭제된 값: " + removeResult);
                 } else {
